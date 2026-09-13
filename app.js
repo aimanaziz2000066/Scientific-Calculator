23af7dca7dad1f44ae2b875cb222bcaf16357f2d
aimanaziz <aimanaziz2000066@gmail.com>
2026-09-13 11:51:19 +0500

    Add app.js file

--- app.js
+++ app.js
@@ -1,90 +1,27 @@
-const display = document.getElementById("display");
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
-function append(value){
-display.value += value;
-}
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
 
-function clearDisplay(){
-display.value="";
-}
-
-function deleteLast(){
-display.value=display.value.slice(0,-1);
-}
-
-function calculate(){
-
-try{
-display.value=eval(display.value);
-}
-catch{
-display.value="Error";
-}
-
-}
-
-function sqrt(){
-
-try{
-display.value=Math.sqrt(eval(display.value));
-}
-catch{
-display.value="Error";
-}
-
-}
-
-function power(){
-
-try{
-display.value=Math.pow(eval(display.value),2);
-}
-catch{
-display.value="Error";
-}
-
-}
-
-function sin(){
-
-try{
-display.value=Math.sin(eval(display.value)*Math.PI/180).toFixed(6);
-}
-catch{
-display.value="Error";
-}
-
-}
-
-function cos(){
-
-try{
-display.value=Math.cos(eval(display.value)*Math.PI/180).toFixed(6);
-}
-catch{
-display.value="Error";
-}
-
-}
-
-function tan(){
-
-try{
-display.value=Math.tan(eval(display.value)*Math.PI/180).toFixed(6);
-}
-catch{
-display.value="Error";
-}
-
-}
-
-function log(){
-
-try{
-display.value=Math.log10(eval(display.value)).toFixed(6);
-}
-catch{
-display.value="Error";
-}
-
-}
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
